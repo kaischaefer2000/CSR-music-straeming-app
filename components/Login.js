4aec1311ac1cee780e2d90817ecdee996852b145
kaischaefer2000 <kai305@web.de>
2022-08-03 21:18:57 +0200

    extract login to be a component

--- components/Login.js
+++ components/Login.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { getProviders, signIn } from 'next-auth/react';
 
-function Login({ providers }) {
+function Login() {
+  const [providers, setProviders] = useState();
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const providers = await getProviders();
+      setProviders(providers);
+    };
+
+    fetchProviders();
+  }, []);
+
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
       <img className="mb-5 w-52" src="https://links.papareact.com/9xl" alt="" />
 
-      {Object.values(providers).map((provider) => (
+      {providers && Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
             className="rounded-full bg-[#18D860] p-5 text-white"
@@ -16,21 +27,8 @@ function Login({ providers }) {
           </button>
         </div>
       ))}
-      
     </div>
   );
 }
 
 export default Login;
-
-
-// get the providers when log in page gets rendered in order to access the spotify Api
-export async function getServerSideProps() {
-  const providers = await getProviders();
-
-  return {
-    props: {
-      providers,
-    },
-  };
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,50 @@
-import { ClientRequest } from 'http'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Sidebar from '../components/Sidebar'
-import Center from '../components/Center'
-import { getSession } from 'next-auth/react'
-import Player from '../components/Player'
+import { ClientRequest } from 'http';
+import Sidebar from '../components/Sidebar';
+import Center from '../components/Center';
+import { getSession, useSession } from 'next-auth/react';
+import Player from '../components/Player';
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { NextPage } from 'next';
+import { getToken } from 'next-auth/jwt';
 
-const Home: NextPage = () => {
+const secret = process.env.JWT_SECRET;
+
+const Home: NextPage = (req) => {
+
+  const token = getToken({ req, secret });
+  console.log(token)
+
+  useEffect(() => {
+    const session = getSession()
+    console.log(session)
+  }, [])
+
   return (
-    <div className='bg-black h-screen overflow-hidden'>
+    <div className="h-screen overflow-hidden bg-black">
       <Head>
         <title>Music Streaming App</title>
       </Head>
-      <main className='flex'>
-        <Sidebar/>
-        <Center/>
-
+      <main className="flex">
+        <Sidebar />
+        <Center />
       </main>
-      <div className='sticky bottom-0'>
+      <div className="sticky bottom-0">
         <Player />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
 
 // prefetch data in order to have access to initial session data
-export async function getServerSideProps(context:any){
-  const session = await getSession(context)
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
 
-  return{
+  return {
     props: {
       session,
-    }
-  }
+    },
+  };
 }
